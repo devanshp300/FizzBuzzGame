8804b7edf165a08c88bd5a075e1baf9ac2b97733
Devansh291001 <devanshpatel2910@icloud.com>
2024-02-28 19:41:21 -0500

    final code

--- src/main/java/org/example/FizzBuzzGame.java
+++ src/main/java/org/example/FizzBuzzGame.java
@@ -14,4 +14,9 @@ public class FizzBuzzGame {
         }
     }
 
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(play(i));
+        }
+    }
 }
