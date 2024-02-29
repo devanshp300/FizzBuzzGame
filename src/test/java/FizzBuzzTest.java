a570be696d4a2a0d45c04744204d3c259e3f4354
Devansh291001 <devanshpatel2910@icloud.com>
2024-02-28 19:20:08 -0500

    Test1

---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void testFizzBuzz_3_ReturnsFizz() {
+        assertEquals("Fizz", FizzBuzzGame.play(3));
+    }
+}
+
